77ba8bd0a1a3d948a020082e00f1290324113e2d
ismailhg <ismailhgndgd@gmail.com>
2025-11-16 15:35:49 +0300

    Project Created
    Project structure created and routing set

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom";
+
+
+export default function Home() {
+  return (
+    <>
+      <h1>TEKNOLOJİK YEMEKLER</h1>
+      <Link to="/order">Sipariş Ver</Link>
+    </>
+  );
+}
---
+++ src/pages/Order.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export default function Order() {
+  return (
+    <>
+      <h1>SİPARİŞ FORM</h1>
+      <Link to="/Success">Sipariş Ver</Link>
+    </>
+  );
+}
